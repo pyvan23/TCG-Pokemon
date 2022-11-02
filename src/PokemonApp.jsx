890e57b1fcfe46e96ea1b65979e35c83d0b06e25
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-01 22:58:16 -0300

    react hokks form

--- src/PokemonApp.jsx
+++ src/PokemonApp.jsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { getPokemons } from "./store/pokemon/thunks";
 import pokemon from "pokemontcgsdk";
+import { getPokemons } from "./store/pokemon/thunks";
 import { NavBar } from "./components/Navbar";
 import { Section } from "./components/Section";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { SearchBar } from "./components/SearchBar";
+
 
 
 export const PokemonApp = () => {
