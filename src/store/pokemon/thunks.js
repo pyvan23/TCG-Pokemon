7f3ad19b3c3ebcc5e3a7e271f22ac395553b04dc
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-11-09 23:03:16 -0300

    yarn lock

--- src/store/pokemon/thunks.js
+++ src/store/pokemon/thunks.js
@@ -7,14 +7,7 @@ export const getPokemons = (page = 1) => {
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons());
 
-    //peticion http
-    // const resp = await fetch(
-    //   `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page * 10}`
-    // );
-    // const data = await resp.json();
-    // const { data } = await pokemonApi(`pokemon?limit=10&offset=${page * 10}`);
-    //  console.log(data);
-    // dispatch(setPokemons({ pokemons: data.results, page: page + 1 }));
+   
 
     pokemon.card.where({ pageSize: 20, page: page })
       .then(result => {
